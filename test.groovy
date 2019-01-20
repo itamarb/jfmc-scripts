4e8b0ad1b10882a77237a9f8d3a01fad7f22fd5a
Itamar <itamarb@jfrog.com>
2019-01-20 15:22:24 +0200

    11

---
+++ test.groovy
@@ -0,0 +1,31 @@
+artifactory('hts-itamarb-db-lb'){
+localRepository('repository-key') {
+  description "Public description"
+  notes "Some internal notes"
+  includesPattern "**/*" // default
+  excludesPattern "" // default
+  repoLayoutRef "maven-2-default"
+  packageType "generic" // "maven" | "gradle" | "ivy" | "sbt" | "nuget" | "gems" | "npm" | "conan" | "helm" |
+                        // "bower" | "debian" | "pypi" | "docker" | "vagrant" | "gitlfs" | "yum" | "generic"
+  debianTrivialLayout false
+  checksumPolicyType "client-checksums" // default | "server-generated-checksums"
+  handleReleases true // default
+  handleSnapshots true // default
+  maxUniqueSnapshots  0 // default
+  snapshotVersionBehavior "unique" // "non-unique" | "deployer"
+  suppressPomConsistencyChecks false // default
+  blackedOut false // default
+  propertySets // (["ps1", "ps2"])
+  archiveBrowsingEnabled false
+  calculateYumMetadata false
+  yumRootDepth 0
+  xrayIndex false
+  blockXrayUnscannedArtifacts false
+  xrayMinimumBlockedSeverity "" // "Minor" | "Major" | "Critical"
+  enableFileListsIndexing ""
+  yumGroupFileNames ""
+  dockerApiVersion "V2" // default
+  maxUniqueTags 0 // default
+  optionalIndexCompressionFormats "" // "bz2", "lzma", "xz"
+}
+}
