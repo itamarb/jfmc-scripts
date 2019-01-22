bf59cdc6a6cf2078554e903b4932944860448cec
Itamar <itamarb@jfrog.com>
2019-01-22 15:49:52 +0200

    22

--- test.groovy
+++ test.groovy
@@ -2,8 +2,7 @@ artifactory('hts-itamarb-db-lb'){
   description userInput (
     type : "STRING",
     value : "This is a generic description",
-    description : "Please provide a description",
-       required: false
+    description : "Please provide a description"
   )
  }
 }
