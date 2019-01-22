26693e16e53f981417799d3481ad5cd9d1f1d4fe
Itamar <itamarb@jfrog.com>
2019-01-22 15:50:49 +0200

    22

--- test.groovy
+++ test.groovy
@@ -1,8 +1,10 @@
 artifactory('hts-itamarb-db-lb'){
+  localRepository("my-repository") {
   description userInput (
     type : "STRING",
     value : "This is a generic description",
-    description : "Please provide a description"
+    description : "Please provide a description",
+       required: false
   )
  }
 }
