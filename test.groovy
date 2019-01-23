252e55c0b71a4e3d59372c565fba293673505daf
Itamar <itamarb@jfrog.com>
2019-01-23 10:14:53 +0200

    23

--- test.groovy
+++ test.groovy
@@ -1,10 +1,17 @@
+exclude = userInput (
+    type: "STRING",
+    value : " ",
+    description : "please provide an exclude pattern"
+  )
+
 artifactory('hts-itamarb-db-lb'){
   localRepository("my-repository") {
   description userInput (
     type : "STRING",
     value : " ",
-    description : "Please provide a description",
-       required: false
+    description : "Please provide a description"
   )
+  includesPattern "**/*" // default
+  excludesPattern exclude // default
  }
 }
