c35a90b4e58c92ef895138e55bf07a811b355333
Itamar <itamarb@jfrog.com>
2019-03-18 14:51:05 +0200

    1

---
+++ test4.groovy
@@ -0,0 +1,21 @@
+instance = userInput (
+  type : "EDGE",
+  multivalued : true,
+  description : "Please select the artifactory instance to run against"
+)
+
+repoName = userInput (
+    type: "STRING",
+    value : "generic-local",
+    description : "please provide a repository name"
+  )
+repoType = userInput (
+  type: "STRING",
+  value : "Maven",
+  description : "please choose a repository type"
+  )
+artifactory(instance){
+   localRepository(repoName) {
+     packageType repoType
+   }
+}
