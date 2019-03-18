d82f90350f6e81e0c849e198a2dae7dcbc3ade25
Itamar <itamarb@jfrog.com>
2019-03-18 14:38:58 +0200

    test

---
+++ test3.groovy
@@ -0,0 +1,38 @@
+repoName = userInput (
+    type: "STRING",
+    value : "generic-local",
+    description : "please provide a repository name"
+  )
+repoType = userInput (
+  type: "STRING",
+  value : "Maven",
+  description : "please choose a repository type"
+  )
+
+artifactory('hts-itamarb-db-lb') {
+  localRepository(repoName) {
+  description "Public description"
+  notes "Some internal notes"
+  includesPattern "**/*" // default
+  excludesPattern "" // default
+  repoLayoutRef "maven-2-default"
+  packageType repoType // "maven" | "gradle" | "ivy" | "sbt" | "nuget" | "gems" | "npm" | "conan" | "helm" |
+                        // "bower" | "debian" | "pypi" | "docker" | "vagrant" | "gitlfs" | "yum" | "generic"
+  checksumPolicyType "client-checksums" // default | "server-generated-checksums"
+  handleReleases true // default
+  handleSnapshots true // default
+  maxUniqueSnapshots  0 // default
+  snapshotVersionBehavior "unique" // "non-unique" | "deployer"
+  suppressPomConsistencyChecks false // default
+  blackedOut false // default
+  propertySets // (["ps1", "ps2"])
+  archiveBrowsingEnabled false
+  calculateYumMetadata false
+  yumRootDepth 0
+  xrayIndex false
+  dockerApiVersion "V2"
+  maxUniqueTags 0
+
+  }
+
+}
